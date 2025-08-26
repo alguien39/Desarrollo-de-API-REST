9cce6a53b4e1561a3376aa1d5f60ed8551afe318
Joham <alguien_bama@hotmail.com>
2025-08-26 09:52:29 -0500

    Prueba

--- Parcial 1/Express/index.js
+++ Parcial 1/Express/index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 app.get('/', (req, res) => {
     res.send('Hola Mundo!');
 });
 
-app.post('/', (req, res) => {
-    res.send('POST Hola Mundo!');
+app.put('/', (req, res) => {
+    res.send('PUT Hola Mundo!');
 });
 
 app.listen(PORT, () => {
-    console.log(`Servidor funcionando http://localhost:${PORT}`);
+    console.log(`Servidor Funcionando http://localhost:${PORT}`);
 });
