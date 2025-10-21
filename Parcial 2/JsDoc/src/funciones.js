2e3c1974e7b6876e7d8fe1deb713f1c61427042e
Joham <alguien_bama@hotmail.com>
2025-10-21 08:39:03 -0500

    Ejercicio De Documentacion

---
+++ Parcial 2/JsDoc/src/funciones.js
@@ -0,0 +1,12 @@
+/**
+ * Funcion que calcula la suma de dos valores A y B
+ * @param {Number} A Numero 1
+ * @param {Number} B Numero 2
+ * @returns {Number} EL resultado de la suma de A y B
+ */
+
+function SumarAB(A,B){
+    return A + B;
+}
+
+export {SumarAB};
---
+++ Parcial 2/JsDoc/src/index.js
@@ -0,0 +1,5 @@
+import { SumarAB } from "./funciones";
+
+let Suma = SumarAB(5,10);
+console.log("La suma es: " + Suma);
+
