fb3035e066cd6436403b8657faa9d6291d477bcf
Joham <alguien_bama@hotmail.com>
2025-11-19 21:37:57 -0600

    Rapidoc Implementation

---
+++ Parcial 3/Rutas De Documentacion/Docs/swagger.js
@@ -0,0 +1,33 @@
+import swaggerJSDoc from "swagger-jsdoc";
+import path from 'path';
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const swaggerYamlPath = path.join(__dirname, 'openapi.yml');
+
+const options = {
+    explorer: true,
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "API de Superhéroes",
+            version: "1.0.0",
+            description: "API para gestionar una lista de superhéroes",
+        },
+        contact: {
+            name: "Joham Barberena",
+            email: "JohamBarberena@hotmail.com"
+        },
+        servers: [
+            {
+                url: "http://localhost:3000",
+                description: "Servidor local",
+            },
+        ],
+    },
+    apis: [swaggerYamlPath],
+};
+
+const specs = swaggerJSDoc(options);
+export default specs;
--- Parcial 3/Rutas De Documentacion/src/index.js
+++ Parcial 3/Rutas De Documentacion/src/index.js
@@ -1,18 +1,46 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import { routerSupers } from './router/supersRouter.js';
-import specs, { customCss } from '../swagger.js';
+import specs from '../Docs/swagger.js';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-app.use("/api-docs",swaggerUi.serve,swaggerUi.setup(specs, undefined, customCss));
+
+app.use("/api-docs",swaggerUi.serve,swaggerUi.setup(specs));
+app.use('/api-docs-json',(req,res)=>{
+  res.json(specs);
+});
+
 app.use('/supers', routerSupers)
 app.use("/api-specs",(req,res)=>{
   res.setHeader("Content-Type","application/json");
   res.send(specs);
 });
+
+app.get('/rapidoc', (req, res) => {
+  res.send(`
+    <!doctype html>
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <script type="module" src="https://unpkg.com/rapidoc/dist/rapidoc-min.js"></script>
+      </head>
+      <body>
+        <rapi-doc spec-url="/api-docs-json"> </rapi-doc>
+      </body>
+    </html>`
+  )
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
